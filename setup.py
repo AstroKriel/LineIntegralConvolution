d472289654fb9213b0443acd47298330d7f3907b
neco kriel <necokriel@gmail.com>
2025-01-02 09:50:33 +1100

    add: file so pypi knows what files to look at

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="line-integral-convolutions",
-    version="1.0.0",
-    url="https://github.com/AstroKriel/line-integral-convolutions/tree/main",
-    description="A script showcasing my implementation for computing line integral convolution.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     author="Neco Kriel",
     author_email="neco.kriel@anu.edu.au",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    version="1.0.3",
+    url="https://github.com/AstroKriel/line-integral-convolutions/tree/main",
+    description="My implementation of line integral convolution (LIC).",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
     ],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     python_requires=">=3.6",
-    install_requires=["numpy", "matplotlib", "scipy", "numba", "scikit-image"],
+    install_requires=open("requirements.txt").read().splitlines(),
 )
