61d8d272dc828f6017053563a1a19b9525f994d7
neco kriel <necokriel@gmail.com>
2025-08-25 17:15:46 +1000

    add utility func to draw cbar

--- src/vegtamr/utils/plots.py
+++ src/vegtamr/utils/plots.py
@@ -20,6 +20,7 @@ def plot_lic(
     ax                  : mpl_axes,
     sfield              : numpy.ndarray,
     vfield              : numpy.ndarray,
+    cmap_name           : str = "twilight",
     bounds_rows         : tuple[float, float] | None = None,
     bounds_cols         : tuple[float, float] | None = None,
     overlay_streamlines : bool = False,
@@ -28,9 +29,9 @@ def plot_lic(
   ):
   if bounds_rows is None: bounds_rows = (0.0, sfield.shape[0])
   if bounds_cols is None: bounds_cols = (0.0, sfield.shape[1]) 
-  ax.imshow(
+  im = ax.imshow(
     sfield,
-    cmap   = "bone",
+    cmap   = cmap_name,
     origin = "lower",
     extent = (
       bounds_rows[0], bounds_rows[1],
@@ -55,6 +56,41 @@ def plot_lic(
   ax.set_yticks([])
   ax.set_xlim(bounds_rows)
   ax.set_ylim(bounds_cols)
+  return im
+
+def add_cbar_from_cmap(
+    ax, mappable,
+    label         : str | None = "",
+    side          : str = "right",
+    percentage    : float = 0.1,
+    cbar_padding  : float = 0.02,
+    label_padding : float = 10,
+    fontsize      : float = 10,
+  ):
+  fig = ax.figure
+  box = ax.get_position()
+  if side in [ "left", "right" ]:
+    orientation = "vertical"
+    cbar_size = box.width * percentage
+    if side == "right":
+      cbar_bounds = [ box.x1 + cbar_padding, box.y0, cbar_size, box.height ]
+    else: cbar_bounds = [ box.x0 - cbar_size - cbar_padding, box.y0, cbar_size, box.height ]
+  elif side in [ "top", "bottom" ]:
+    orientation = "horizontal"
+    cbar_size = box.height * percentage
+    if side == "top":
+      cbar_bounds = [ box.x0, box.y1 + cbar_padding, box.width, cbar_size ]
+    else: cbar_bounds = [ box.x0, box.y0 - cbar_size - cbar_padding, box.width, cbar_size ]
+  else: raise ValueError(f"Unsupported side: {side}")
+  ax_cbar = fig.add_axes(cbar_bounds)
+  cbar = fig.colorbar(mappable=mappable, cax=ax_cbar, orientation=orientation)
+  if orientation == "horizontal":
+    cbar.ax.set_title(label, fontsize=fontsize, pad=label_padding)
+    cbar.ax.xaxis.set_ticks_position(side)
+  else:
+    cbar.set_label(label, fontsize=fontsize, rotation=-90, va="bottom")
+    cbar.ax.yaxis.set_ticks_position(side)
+  return cbar
 
 
 ## END OF MODULE
